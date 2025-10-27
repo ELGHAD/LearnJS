20e3c4d6b4aba48f351efce3c88c230878894cd7
Hamza <hamelrhadiouini@gmail.com>
2025-10-27 16:47:45 +0100

    import/export

---
+++ import/export/calc.js
@@ -0,0 +1,5 @@
+export const add = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
+export default function hello() {
+  console.log("Welcome to Calc Module");
+}
---
+++ import/export/main.js
@@ -0,0 +1,5 @@
+import hello, { add, multiply } from "./calc.js";
+
+hello();
+console.log(add(2, 5));
+console.log(multiply(3, 4));
